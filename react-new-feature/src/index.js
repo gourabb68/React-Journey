4f2f5620f4a0423b21b091758be8651f1a830fdb
gourabb68 <gourabb68@gmail.com>
2020-06-24 17:02:24 +0530

    complex state maintain like array of object using useState

--- react-new-feature/src/index.js
+++ react-new-feature/src/index.js
@@ -2,32 +2,97 @@ import React,{useState} from 'react';//useState hook
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = () =>{
-const [count,setCount]=useState(0);
 
-const increment = ()=>{
-  setCount(count+1);
-}
-const decrement = ()=>{
-  setCount(count-1);
-}
-const reset = ()=>{
-  setCount(0);
-}
-  return (
-    <div>
-      <p> The current count is {count}</p>
-      <button onClick={increment}> +1</button>
-      <button onClick={decrement}> -1</button>
-      <button onClick={reset}> reset</button>
+const NoteApp =()=>{
+  const [notes, setNotes]=useState([]);
+  const [title,setTitle]=useState('');
+  const [body,setBody]=useState('');
+  const addNote =(e)=>{
+       e.preventDefault();
+       setNotes([
 
-    </div>
+         ...notes,
+         {
+           title,body
+         }
+        
+       ])
+       setTitle('')
+  }
+  const remove =(title,body)=>{
+    // console.log('rm'+title)
+    setNotes(notes.filter((note)=>note.title!==title))
+    setBody(notes.filter((note)=>note.body!==body))
+  }
+  return(
+     <div>
+       <h1>Notes</h1>
+       {notes.map((note)=>(
+         <div key={note.title}>
+           <h3>{note.title}</h3>
+           <h3>{note.body}</h3>
+           <button onClick={()=>remove(note.title,note.body)}>Remove</button>
+           </div>
+       ))}
+       <p>Add Note</p>
+       <form onSubmit={addNote}>
+         <input value={title} onChange={(e)=> setTitle(e.target.value)}/>
+         <textarea placeholder=
+                    'Add a note for your expense'
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}></textarea>
+         <button > Add Note</button>
+       </form>
+     </div>
   )
 }
 
+// //trying to create state as class using singl useState call
+// const App = (props) =>{
+//   const [state,setState]=useState({
+//     count: props.count,
+//     text: ''
+//   })
 
+//     return (
+//       <div>
+//         <p> The current {state.text ||'count'} is {state.count}</p>
+//         <button onClick={()=>  setState({count: state.count+1})}> +1</button>
+//         <button onClick={() =>  setState({count: state.count-1})}> -1</button>
+//         <button onClick={()=>  setState({count: state.count})}> reset</button>
+//         <input value={state.text} onChange={(e)=> setState({text: e.target.value})}></input>
+//       </div>
+//     )
+//   }
+  
+// const App = (props) =>{
+// const [count,setCount]=useState(props.count);
+// const [text,setText]=useState('');//2nd state value
+// const increment = ()=>{
+//   setCount(count+1);
+// }
+// const decrement = ()=>{
+//   setCount(count-1);
+// }
+// const reset = ()=>{
+//   setCount(props.count);
+// }
+//   return (
+//     <div>
+//       <p> The current {text || count} is {count}</p>
+//       <button onClick={increment}> +1</button>
+//       <button onClick={decrement}> -1</button>
+//       <button onClick={reset}> reset</button>
+//       <input value={text} onChange={(e)=> setText(e.target.value)}></input>
+//     </div>
+//   )
+// }
+
+// App.defaultProps ={
+//   count:10
+// }
 ReactDOM.render(
- <App />,
+ <NoteApp />,
   document.getElementById('root')
 );
 
